6c58707fae7d69f193d73d0bae787f62243b108f
Caio Rodrigues <caiounderscore@protonmail.com>
2020-11-06 19:28:58 -0300

    00

---
+++ 00/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	numBytes, err := fmt.Println("Hello World")
+	fmt.Println(numBytes, err)
+
+	//gopher (:=) works only inside code blocks e and define automatically variable types
+	a, b, c, d := 0, -1.0, "string", true
+	fmt.Println("Golang primitive types:", reflect.TypeOf(a), reflect.TypeOf(b), reflect.TypeOf(c), reflect.TypeOf(c))
+
+	// = use to insert new values on variables already existences
+	a, b, c, d = -1, 10, "string", true
+	fmt.Printf("Golang primitive types: %T, %T, %T, %T", a, b, c, d)
+}
