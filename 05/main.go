f4675c62e7de409df5ba41c839333d462157e07f
Caio <caiounderscore@protonmail.com>
2020-12-02 11:37:14 -0300

    05

---
+++ 05/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+   "fmt"
+)
+
+var x int
+
+func main() {
+
+    x := 200
+    fmt.Printf("%d\t%b\t%#x\n", x, x, x)
+    y := x << 1
+    fmt.Printf("%d\t%b\t%#x", y, y, y)
+
+}
+
